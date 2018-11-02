fcaf4dfb6c5e9e6c12284ebf4fbdfd66cc9a01c9
baixiaojian <15101661380@163.com>
2018-11-02 10:08:12 +0800

    update router config

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/common/Home'
 import Index from '@/components/page/Index'
-import SystemManage from '@/components/page/SystemManage'
 
 import RegistManage from '@/components/page/RegistManage'
 import RegistDetail from '@/components/page/RegistDetail'
@@ -19,6 +18,19 @@ import Login from '@/components/page/Login'
 import DataDetailsTaskList from '@/components/page/DataDetailsTaskList'
 import DataDetailsRecommendList from '@/components/page/DataDetailsRecommendList'
 import DataDetailsRewardList from '@/components/page/DataDetailsRewardList'
+
+import SystemView from '@/components/page/system'
+import SystemLimits from '@/components/page/system/children/Limits'
+import SystemLog from '@/components/page/system/children/Log'
+import RoleManage from '@/components/page/system/children/RoleManage'
+import UserManage from '@/components/page/system/children/UserManage'
+
+import StaticView from '@/components/page/statistics'
+import StaticDetail from '@/components/page/statistics/children/detail'
+import StaticTask from '@/components/page/statistics/children/task'
+import StaticCommend from '@/components/page/statistics/children/commend'
+import StaticAward from '@/components/page/statistics/children/award'
+
 Vue.use(Router)
 
 export default new Router({
@@ -26,6 +38,7 @@ export default new Router({
   // base: __dirname,
   routes: [{
     path: '/home',
+    name: 'home',
     component: Home,
     children: [
       {
@@ -34,60 +47,126 @@ export default new Router({
       },
       {
         path: '/Index',
+        name: 'index',
         component: Index
       },
       {
-        path: '/systemManage',
-        component: SystemManage
+        path: '/system',
+        component: SystemView,
+        children: [
+          {
+            path: '',
+            component: SystemLimits
+          },
+          {
+            path: 'limits',
+            name: 'limits',
+            component: SystemLimits
+          },
+          {
+            path: 'log',
+            name: 'log',
+            component: SystemLog
+          },
+          {
+            path: 'role',
+            name: 'role',
+            component: RoleManage
+          },
+          {
+            path: 'user',
+            name: 'user',
+            component: UserManage
+          }
+        ]
+      },
+      {
+        path: '/statistics',
+        component: StaticView,
+        children: [
+          {
+            path: 'detail',
+            name: 'detail',
+            component: StaticDetail
+          },
+          {
+            path: 'task',
+            name: 'task',
+            component: StaticTask
+          },
+          {
+            path: 'commend',
+            name: 'commend',
+            component: StaticCommend
+          },
+          {
+            path: 'award',
+            name: 'award',
+            component: StaticAward
+          }
+        ]
       },
       {
         path: '/RegistManage',
+        name: 'registmanage',
         component: RegistManage
       },
       {
         path: '/RegistDetail',
+        name: 'registdetail',
         component: RegistDetail
       },
       {
         path: '/TaskManage',
+        name: 'taskmanage',
         component: TaskManage
       },
       {
         path: '/DataStatics',
+        name: 'datastatics',
         component: DataStatics
       },
       {
         path: '/DataDetailsTaskList',
+        name: 'datadetailstasklist',
         component: DataDetailsTaskList
 
       }, {
         path: '/DataDetailsRecommendList',
+        name: 'datadetailsrecommendlist',
         component: DataDetailsRecommendList
 
       },
       {
         path: '/DataDetailsRewardList',
+        name: 'datadetailsrewardlist',
         component: DataDetailsRewardList
 
       },
       {
         path: '/DataStatisDetail',
+        name: 'datastatisdetail',
         component: DataStatisDetail
       }, {
-        path: '/DeployManage',
+        path: '/deploymanage',
+        name: 'deploymanage',
         component: DeployManage
       }, {
         path: '/TaskDetails',
+        name: 'taskdetails',
         component: TaskDetails
       }, {
         path: '/TaskNew',
+        name: 'tasknew',
         component: TaskNew
       }, {
         path: '/TaskEdit',
+        name: 'taskedit',
         component: TaskEdit
       },
       {
         path: '/TaskEntryForm',
+        name: 'taskentryform',
         component: TaskEntryForm
       }
     ]
