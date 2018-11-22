0c70c448c0851ce7b6459a3588e5fcc357dd4a62
baixiaojian <15101661380@163.com>
2018-11-22 18:05:44 +0800

    fix reward bill

--- src/components/page/statistics/children/Bill.vue
+++ src/components/page/statistics/children/Bill.vue
@@ -84,7 +84,7 @@
       </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button size="mini" disabled="disabled" @click="sendReward(scope.row)">发放奖励</el-button>
+          <el-button size="mini" :disabled="scope.row.lssuingstate === '已发放'" @click="sendReward(scope.row)">发放奖励</el-button>
         </template>
       </el-table-column>
     </el-table>
